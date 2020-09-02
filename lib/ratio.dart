234c8f7a256f7ab631ab64726d7234357a5a8290
antklim <antklim@gmail.com>
2020-09-02 20:34:06 +1000

    Ratio calculator interface defined

---
+++ lib/ratio.dart
@@ -0,0 +1,25 @@
+/// ratio returns ratio of coffee and water required to brew.
+///   - coffee - weight of coffee in grams
+///   - water - volume of water in ml
+double ratio(double coffee, double water) {
+  // 1. coffee weight should be positive
+  // 2. water volume should be positive
+}
+
+/// coffeePerWater returns coffee in grams required to brew for provided amount
+/// of water.
+///   - ratio - brewing ratio
+///   - water - volume of water in ml
+double coffeePerWater(double ratio, double water) {
+  // 1. ratio should be positive
+  // 2. water volume should be positive
+}
+
+/// waterPerCoffee returns volume of water in ml required to brew provided
+/// weight of coffee in grams.
+///   - ratio - brewing ratio
+///   - coffee - weight of coffee in grams
+double waterPerCoffee(double ratio, double coffee) {
+  // 1. ratio should be positive
+  // 2. coffee weight should be positive
+}
---
+++ test/ratio_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('ratio calculator', () {
+    test('throws when coffee weight is not positive', () {});
+    test('throws when water volume is not positive', () {});
+  });
+}
