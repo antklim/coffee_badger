20d8bf23e1fd88b027471207f7f2fa530681ea11
antklim <antklim@gmail.com>
2020-10-08 21:42:04 +1100

    Making app theme colors public

--- lib/app_theme.dart
+++ lib/app_theme.dart
@@ -7,43 +7,43 @@ class AppTheme {
   // Primary colors map to components and elements, like app bars and buttons.
   // Secondary colors are most often used as accents on components, such as FABS and selection controls.
   // Color variants used to complement and provide accessible options for your primary and secondary colors.
-  static const Color _primaryColor = Color(0xFFCA9865);
-  static const Color _primaryVariantColor = Color(0xFF130904);
-  static const Color _secondaryColor = Color(0xFF892E0D);
-  static const Color _secondaryVariantColor = Color(0xFF892E0D);
+  static const Color primaryColor = Color(0xFFCA9865);
+  static const Color primaryVariantColor = Color(0xFF130904);
+  static const Color secondaryColor = Color(0xFF892E0D);
+  static const Color secondaryVariantColor = Color(0xFF892E0D);
 
   // Background color is found behind scrollable content.
-  static const Color _backgroundColor = Color(0xFFEFEDED);
+  static const Color backgroundColor = Color(0xFFEFEDED);
   // Surface colors map to components such as cards, sheets, and menus.
-  static const Color _surfaceColor = Color(0xFFCAB9B8);
+  static const Color surfaceColor = Color(0xFFCAB9B8);
   // Error color indicates errors in components, such as text fields.
-  static const Color _errorColor = Color(0xFFB00020);
+  static const Color errorColor = Color(0xFFB00020);
 
   // “On” colors are primarily applied to text, iconography, and strokes.
-  static const Color _onPrimaryColor = Color(0xFFEFEDED);
-  static const Color _onSecondaryColor = Color(0xFFEFEDED);
-  static const Color _onBackgroundColor = Color(0xFF892E0D);
-  static const Color _onSurfaceColor = Color(0xFF424242);
-  static const Color _onErrorColor = Color(0xFFEFEDED);
+  static const Color onPrimaryColor = Color(0xFFEFEDED);
+  static const Color onSecondaryColor = Color(0xFFEFEDED);
+  static const Color onBackgroundColor = Color(0xFF892E0D);
+  static const Color onSurfaceColor = Color(0xFF424242);
+  static const Color onErrorColor = Color(0xFFEFEDED);
 
   static final ThemeData appTheme = ThemeData(
     colorScheme: ColorScheme.light(
-      primary: _primaryColor,
-      primaryVariant: _primaryVariantColor,
-      secondary: _secondaryColor,
-      secondaryVariant: _secondaryVariantColor,
-      surface: _surfaceColor,
-      background: _backgroundColor,
-      error: _errorColor,
-      onPrimary: _onPrimaryColor,
-      onSecondary: _onSecondaryColor,
-      onSurface: _onSurfaceColor,
-      onBackground: _onBackgroundColor,
-      onError: _onErrorColor,
+      primary: primaryColor,
+      primaryVariant: primaryVariantColor,
+      secondary: secondaryColor,
+      secondaryVariant: secondaryVariantColor,
+      surface: surfaceColor,
+      background: backgroundColor,
+      error: errorColor,
+      onPrimary: onPrimaryColor,
+      onSecondary: onSecondaryColor,
+      onSurface: onSurfaceColor,
+      onBackground: onBackgroundColor,
+      onError: onErrorColor,
       brightness: Brightness.light,
     ),
     buttonTheme: ButtonThemeData(
-      buttonColor: _primaryColor,
+      buttonColor: primaryColor,
     ),
     fontFamily: 'Roboto',
     textTheme: _textTheme,
@@ -54,82 +54,82 @@ class AppTheme {
       InputDecorationTheme(
           border: InputBorder.none,
           focusedBorder: UnderlineInputBorder(
-              borderSide: BorderSide(color: _secondaryColor)));
+              borderSide: BorderSide(color: secondaryColor)));
 
   // Use https://material.io/design/typography/the-type-system.html#type-scale to build TextTheme
   static final TextTheme _textTheme = TextTheme(
     headline1: TextStyle(
-      color: _onBackgroundColor,
+      color: onBackgroundColor,
       fontSize: 96,
       fontWeight: FontWeight.w300,
       letterSpacing: -1.5,
     ),
     headline2: TextStyle(
-      color: _onBackgroundColor,
+      color: onBackgroundColor,
       fontSize: 60,
       fontWeight: FontWeight.w300,
       letterSpacing: -0.5,
     ),
     headline3: TextStyle(
-      color: _onBackgroundColor,
+      color: onBackgroundColor,
       fontSize: 48,
       fontWeight: FontWeight.w400,
     ),
     headline4: TextStyle(
-      color: _onBackgroundColor,
+      color: onBackgroundColor,
       fontSize: 34,
       fontWeight: FontWeight.w400,
       letterSpacing: 0.25,
     ),
     headline5: TextStyle(
-      color: _onBackgroundColor,
+      color: onBackgroundColor,
       fontSize: 24,
       fontWeight: FontWeight.w400,
     ),
     headline6: TextStyle(
-      color: _onBackgroundColor,
+      color: onBackgroundColor,
       fontSize: 20,
       fontWeight: FontWeight.w500,
       letterSpacing: 0.15,
     ),
     subtitle1: TextStyle(
-      color: _onSurfaceColor,
+      color: onSurfaceColor,
       fontSize: 16,
       fontWeight: FontWeight.w400,
       letterSpacing: 0.15,
     ),
     subtitle2: TextStyle(
-      color: _onSurfaceColor,
+      color: onSurfaceColor,
       fontSize: 14,
       fontWeight: FontWeight.w500,
       letterSpacing: 0.1,
     ),
     bodyText1: TextStyle(
-      color: _onBackgroundColor,
+      color: onBackgroundColor,
       fontSize: 16,
       fontWeight: FontWeight.w400,
       letterSpacing: 0.5,
     ),
     bodyText2: TextStyle(
-      color: _onBackgroundColor,
+      color: onBackgroundColor,
       fontSize: 14,
       fontWeight: FontWeight.w400,
       letterSpacing: 0.25,
     ),
     button: TextStyle(
-      color: _onPrimaryColor,
+      color: onPrimaryColor,
       fontSize: 14,
       fontWeight: FontWeight.w500,
       letterSpacing: 1.25,
     ),
     caption: TextStyle(
-      color: _onSurfaceColor,
+      color: onSurfaceColor,
       fontSize: 12,
       fontWeight: FontWeight.w400,
       letterSpacing: 0.4,
     ),
     overline: TextStyle(
-      color: _onSurfaceColor,
+      color: onSurfaceColor,
       fontSize: 10,
       fontWeight: FontWeight.w400,
       letterSpacing: 1.5,
